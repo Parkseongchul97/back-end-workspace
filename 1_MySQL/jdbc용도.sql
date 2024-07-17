2de3874b2c0d583f8589eeacdfbc4deda43f4481
Parkseongchul97 <dol9991@naver.com>
2024-07-17 17:50:32 +0900

    Update jdbc용도.sql
    20240717

--- 1_MySQL/jdbc용도.sql
+++ 1_MySQL/jdbc용도.sql
@@ -7,10 +7,16 @@ CREATE TABLE person(
 
 SELECT * FROM person; 
 CREATE TABLE member(
-	id VARCHAR(30) ,
+	id VARCHAR(30) PRIMARY KEY ,
     password VARCHAR(30),
 	name VARCHAR(10)
 );
+Drop table member;
 
 
 SELECT * FROM member;
+
+
+
+INSERT INTO member
+VALUES ('qwer1234', '1234', '김철수');
